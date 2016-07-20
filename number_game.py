84dd9c78e2cffc106b82d12bebbbfe4f681f7cb0
Line Møller <linemoller@Lines-MacBook-Air.local>
2016-07-20 11:29:58 +0200

    Initial commit

---
+++ number_game.py
@@ -0,0 +1,20 @@
+from random import randint
+
+number = randint(1,25)
+
+def try_number(test_number):
+	test_number = int(test_number)
+	if (test_number == number):
+		return 'Correct! Good Job!'
+	elif (test_number < number):
+		print('Not quite, go higher')
+		return try_number(input())
+	else:
+		print('Not quite, go lower')
+		return try_number(input())
+
+print("Can you guess the number I'm thinking of?")
+print(try_number(input()))
+
+
+#git test 1st change
